eb580517bc95ecd52a0120f9f9b06af1c34ee5c2
ayushrai26 <93040683+ayushrai26@users.noreply.github.com>
2025-08-20 22:54:08 +0530

    Updated server.js with Render configuration

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use(express.json())
 app.use('/upload',upload)
 app.use('/',userRoutes)
 app.use('/admin',adminRoutes)
-mongoose.connect('mongodb://localhost:27017/trackHire')
-.then(()=>console.log('Connected to database'))
-.catch((err)=>console.error('connection error',err))
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
 app.listen(PORT,()=>
     console.log('connected')
 )
